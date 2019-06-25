11ba093e6e03310f5ba281d1f7d660c5911f0cb5
17699a0521 <51588666+17699a0521@users.noreply.github.com>
2019-06-25 16:02:38 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,7 +1,7 @@
 //installation
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open("sample").then((cache) => {
+    caches.open("restaurant-v1").then((cache) => {
       return cache.addAll([]);
     })
   )
@@ -12,7 +12,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((res) => {
       return res || fetch(event.request).then((response) => {
-        return caches.open("sample").then((cache) => {
+        return caches.open("restaurant-v1").then((cache) => {
           cache.put(event.request, response.clone());
           return response;
         })
@@ -21,3 +21,17 @@ self.addEventListener("fetch", (event) => {
     })
   )
 })
+
+self.addEventListener('activate', function(event) {
+  var newCache = ['restaurant-v2'];
+
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (newCache.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
