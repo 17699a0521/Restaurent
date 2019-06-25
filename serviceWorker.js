fefbdaab08167e3e09c9247f4104773662029a7f
17699a0521 <51588666+17699a0521@users.noreply.github.com>
2019-06-25 15:57:03 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -12,7 +12,7 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((res) => {
       return res || fetch(event.request).then((response) => {
-        caches.open("sample").then((cache) => {
+        return caches.open("sample").then((cache) => {
           cache.put(event.request, response.clone());
           return response;
         })
