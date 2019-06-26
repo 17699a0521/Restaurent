471467b68649bbabb879fe37ae7754424887688e
17699a0521 <51588666+17699a0521@users.noreply.github.com>
2019-06-26 15:07:47 +0530

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,38 +1,39 @@
-//installation
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open("restaurant-v1").then((cache) => {
-      return cache.addAll([
-      ]);
-    })
-  )
+    
+// installation
+self.addEventListener("install", (event)=>{
+	event.waitUntil(
+    caches.open("restaurant-v1").then((cache)=> {
+		return cache.addAll([
+			'./',
+			'./index.html',
+			'./restaurant.html',
+			'./serviceWorker.js',
+			'./js/dbhelper.js',
+			'./js/main.js',
+			'./js/restaurant_info.js',
+			'./css/styles.css',
+			'./data/restaurants.json',
+			'./img/1.jpg',
+			'./img/2.jpg',
+			'./img/3.jpg',
+			'./img/4.jpg',
+			'./img/5.jpg',
+			'./img/6.jpg',
+			'./img/7.jpg',
+			'./img/8.jpg',
+			'./img/9.jpg',
+			'./img/10.jpg'
+		]);
+	}))
 })
-
-//Fetch event
+// fetch event
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((res) => {
-      return res || fetch(event.request).then((response) => {
-        return caches.open("restaurant-v1").then((cache) => {
-          cache.put(event.request, response.clone());
-          return response;
-        })
-      })
-
-    })
-  )
+	event.respondWith(caches.match(event.request).then((respons) => {
+		return respons || fetch(event.request).then((response) => {
+			return caches.open("restaurant-v1").then((cache) => {
+				cache.put(event.request, response.clone());
+				return response;
+			})
+		})
+	}))
 })
-
-self.addEventListener('activate', function(event) {
-  var newCache = ['restaurant-v2'];
-
-  event.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (newCache.indexOf(key) === -1) {
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-});
